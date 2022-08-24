a55662dbb96121079bcdad1e7c63769887e84b7a
alif kamate <92592922+alip533@users.noreply.github.com>
2022-08-24 15:21:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors: {
+      Soft_blue: 'hsl(215 ,51% 70% )',
+      Cyan: 'hsl(178, 100 %, 50 %)',
+      veryDarkBlue: 'hsl(217, 54%, 11%)',
+      card_BG: 'hsl(216, 50%, 16%)',
+      line: 'hsl(215, 32%, 27%)',
+      white: ' hsl(0, 0%, 100%)'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
